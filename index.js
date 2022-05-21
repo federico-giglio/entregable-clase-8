8ced6a3a0c6b0909f3af7c88846f98b801674137
Federico Giglio <federicoagustingiglio@gmail.com>
2022-05-20 21:43:53 -0300

    first commit

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 
-//importamos express
+// Importamos
 const express = require('express');
-//importamos contenedor
-const contenedor = require('./Contenedor')
-//invocamos express()
 const app = express()
-//invocamos productos
+const contenedor = require('./Contenedor')
 const productos = new contenedor ('./productos.txt')
 
-app.get('/', (req, res) => {
-    res.send('<div><a href="/productos">Ver productos</a><br><a href="/productoRandom">Ver producto al azar</a></div>'
-    )
-})
-//
-app.get('/productos', async (req, res) => {
-    const productitos = await productos.getAll()
-    res.send(productitos);
-})
+app.use(express.static('public'))
 
-app.get('/productoRandom', async (req, res) => {
-    const productitoRandom = await productos.randomize()
-    res.send(productitoRandom)
-})
 
-//método listen para escuchar puerto 8080
+
+
+
+
+// Server
 app.listen(8080, () => {
-    console.log('server listening...');
+    console.log('Escuchando')
 })
